c52b9a093cc1a18efbd6c11eb6991747700ebfbf
Krishu-546 <krishadabhi90@gmail.com>
2026-02-12 18:56:21 +0530

    Add movie recommendation function and main interface

--- Task-3_RecommendationSystem/recommendation.py
+++ Task-3_RecommendationSystem/recommendation.py
@@ -1,1 +1,30 @@
+def recommend_movie(genre):
+
+    recommendations = {
+        "action" : ["Avengers", "John Wick", "Mad Max"],
+        "comedy" : ["3 Idiots", "The Mask", "Hera Pheri"],
+        "romantic" : ["Titanic", "The Nptebook", "DDLJ"],
+        "horror" : ["Conjuring", "Insidious", "It"]
+    }
+
+    genre = genre.lower()
+
+    if genre in recommendations:
+        print("\nRecommended Movies:")
+        for movie in recommendations[genre]:
+            print("-", movie)
+    else:
+        print("Sorry! Genre not found.")
+
+def main():
+    print("🎬 Movie Recommendation System")
+    print("Available genres: Action, Comedy, Romantic, Horror")
+
+    user_genre = input("Enter your favorite genre: ")
+    recommend_movie(user_genre)
+
+if __name__ == "__main__":
+    main()
+
+
 
